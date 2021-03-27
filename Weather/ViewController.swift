f014bd54318ceb51fb511792c6e5ecdfdd36223d
Admin <admin@Admins-MacBook-Pro.local>
2021-03-27 11:58:53 +0530

    Added project files and updated .gitignore

---
+++ Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Admin on 27/03/21.
+//  Copyright © 2021 Satish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
